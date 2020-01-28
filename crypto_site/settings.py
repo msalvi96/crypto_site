63e07f830e8e837a2594df7cd9ce789c33b0c838
Mrunal <msalvi96@stevens.edu>
2020-01-27 21:35:48 -0500

    Fixed runtime

--- crypto_site/settings.py
+++ crypto_site/settings.py
@@ -122,3 +122,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 # STATIC_ROOT = "./crypto/static/"
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
